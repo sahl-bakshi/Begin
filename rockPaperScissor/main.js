0fd1fe713923e33cabb417073f8c873a6bd5fb4a
Sahl Bakshi <sahlaajazbakshi@gmail.com>
2021-07-10 04:10:56 +0300

    final version

--- rockPaperScissor/main.js
+++ rockPaperScissor/main.js
@@ -1,15 +1,15 @@
-const ROCK = "rock";
-const PAPER = "paper";
-const SCISSOR = "scissor";
+// constansts
+const OPTIONS = ["rock", "paper", "scissor"];
+
+// variables
 let playerScore = 0;
 let computerScore = 0;
 
-// 1
+// 1 
 
 function computerPlay() {
     let between = 3;
-    let options = [ROCK, PAPER, SCISSOR];
-    return options[math.floor(math.random() * between)];
+    return math.floor(math.random() * between);
 }
 
 // 2
@@ -17,25 +17,11 @@ function computerPlay() {
 function playRound(player, computer) {
     let num;
     if (player == computer) {
-        num = 0;
-    } else if (player == ROCK) {
-        if (computer == PAPER) {
-            num = 1;
-        } else if (computer == SCISSOR) {
-            num = 2;
-        }
-    } else if (player == PAPER) {
-        if (computer == ROCK) {
-            num = 2;
-        } else if (computer == SCISSOR) {
-            num = 1;
-        }
-    } else if (player == SCISSOR) {
-        if (computer == ROCK) {
-            num = 1;
-        } else if (computer == PAPER) {
-            num = 2;
-        }
+        num = 3;
+    } else if ((player == 0 && computer == 2) || player == computer+1) {
+        num = 2;
+    } else {
+        num = 1;
     }
     return output(num, player, computer);
 }
@@ -44,42 +30,58 @@ function playRound(player, computer) {
 
 const rockButton = document.querySelector(".rockButton");
 rockButton.addEventListener('click', function() {
-    playRound(ROCK, computerPlay());
+    playRound(0, computerPlay());
 });
 
 const paperButton = document.querySelector(".paperButton");
 paperButton.addEventListener('click', function() {
-    playRound(PAPER, computerPlay());
+    playRound(1, computerPlay());
 });
 
 const scissorButton = document.querySelector(".scissorButton");
 scissorButton.addEventListener('click', function() {
-    playRound(SCISSOR, computerPlay());
+    playRound(2, computerPlay());
 });
 
 // 4
 
-function disableButtons() {
-    document.querySelector(".rockButton").disabled = true;
-    document.querySelector(".paperButton").disabled = true;
-    document.querySelector(".scissorButton").disabled = true;
-}  
-
-// 5
-
 function output(num, player, computer) {
-    
     if (num == 2) {
         ++playerScore;
     } else if (num == 1) {
         ++computerScore
     }
-    document.querySelector("h2").innerHTML = playerScore + " " + computerScore;
+    const score = document.querySelector(".score");
+    score.innerHTML = playerScore + " " + computerScore;
     
-    if (playerScore >= 5) {
+    if (playerScore >= 5 || computerScore >= 5) {
         disableButtons();
-    } else if (computerScore >= 5) {
-        disableButtons();
-    } 
-    document.querySelector("p").innerHTML = player + " " + computer;
+        reload();
+    }
+    const track = document.querySelector(".track");
+    track.innerHTML = OPTIONS[player] + " " + OPTIONS[computer];
+}
+
+// 5
+
+function disableButtons() {
+    document.querySelector(".rockButton").disabled = true;
+    document.querySelector(".paperButton").disabled = true;
+    document.querySelector(".scissorButton").disabled = true;
+}
+
+// 6
+
+function reload() {
+    const div = document.createElement("div");
+    document.body.append(div);
+    div.classList.add("position");
+    
+    const btn = document.createElement("button");
+    div.appendChild(btn);
+    btn.classList.add("restart");
+    btn.innerHTML = "restart";
+    btn.addEventListener('click', function() {
+        window.location.reload();
+    }); 
 }
