1c91101d7f480abe8516fa79b7e7850b4037692c
Sahl Bakshi <sahlaajazbakshi@gmail.com>
2022-01-10 00:47:16 +0300

    added button

--- library/main.js
+++ library/main.js
@@ -11,20 +11,55 @@ const harry = new Book("Harry", "Rowling", 243, true);
 const potts = new Book("Potts", "Rowling", 249, false);
 
 
-const body = document.querySelector("body");
+// query selectors
 const image = document.querySelector(".image");
+const header = document.querySelector("h1");
+const body = document.querySelector("body");
+const interface = document.querySelector(".interface");
 const button = document.querySelector(".button");
+const wrapIntr = document.querySelector(".wrapUI");
+const wrapForm = document.querySelector(".wrapForm");
+const submit = document.getElementById("submit");
+
+// make remove button
+const remove = document.createElement("button");
+
+// change read status
+const readStatus = document.createElement("button");
+
+
+
+// event listeners
 
 button.addEventListener('click', () => {
-    image.remove();
-    button.classList.add("right");
-    addBook();
+    if (library.length == 0) {
+        image.remove();
+        header.style.margin = "0px";
+        button.style.margin = "0px";
+        interface.classList.add("update");
+    }
+    wrapForm.style.display = "flex";
+    button.style.display = "none";
+})
+
+submit.addEventListener("click", function(event) {
+    event.preventDefault(); // PREVENTS FORM FROM AUTO SUBMITTING
+    addToLibrary();
+    wrapForm.style.display = "none";
+    button.style.display = "flex";
+    displayBooks();
 })
 
 
-function addBook() {
-    const form = document.createElement('form');
-    body.appendChild(form);
+function addToLibrary() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+    let book = new Book(title, author, pages, read);
+    library.push(book);
+}
 
-    // labelsr
+function displayBooks() {
+
 }
